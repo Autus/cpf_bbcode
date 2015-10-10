dfc3bb52ec9d2f49e2d77529d13f28577aca13eb
Autus <kaelynburns@gmail.com>
2015-10-10 00:48:28 -0400

    Create ext.php

---
+++ ext.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * 
+ * Parse BBCode in Custom Profile Fields
+ * 
+ * @copyright (c) 2015 Autus/Sequor ( http://autus.dekoryn.com/ )
+ * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+ * @author Kaelyn Burns (Autus/Sequor)
+ */
+
+namespace sequor\cpf_bbcode;
+
+class ext extends \phpbb\extension\base
+{
+}
